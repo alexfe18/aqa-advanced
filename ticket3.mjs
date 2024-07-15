8e179789786480c70d1c45ebb10c7e748afe47d7
alexfe18 <alex.fedorchuk@ffwagency.com>
2024-07-15 16:18:01 +0300

    Add files via upload
    task2_b

--- ticket3.mjs
+++ ticket3.mjs
@@ -1,5 +1,5 @@
 function fetchUser() {
-    return fetch('https://jsonplaceholder.typicode.com/users/1')
+    return fetch('https://jsonplaceholder.typicode.com/users/9')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
